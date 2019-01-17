ca4f676b893972698982a431de3107f4cc0b6d92
Tommy Tran <tranttommy95@gmail.com>
2019-01-16 20:42:16 -0800

    added functions to main.js and linked to index.html

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-console.log("Hallo thar");
+function sayHello() {
+    console.log('SUP BRUH');
+}
+
+function addX() {
+    x++;
+}
+
+function sayX(){
+    console.log(x);
+}
+
+function evenOrOdd() {
+    var input = document.getElementById('number');
+    console.log(input.value + 2);
+
+    var y = input.value;
+    
+    console.log(y + 2);
+    
+    if(y % 2 === 0) {
+        console.log('Even');
+    } else if(y % 2 === 1) {
+        console.log('Odd');
+    } else {
+        console.log('Not a number');
+    }
+}
+
+var x = 0;
+/*
+if(x > 12) {
+    console.log(x);
+} else {
+    console.log('Sorry, ' + x + ' is too small...');
+}
+
+if(x > 15) {    
+    for(var i = 0; i < x; i++) {
+        if(i % 2 === 0) {
+            console.log(i);
+        } else {
+            console.log('ODD NUMBER')
+        }
+    }
+}
+*/
